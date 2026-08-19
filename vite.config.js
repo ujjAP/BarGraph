813a74b716e07c9d18ed435cd8d127afa67f6101
Ujjwal <316460362+ujjAP@users.noreply.github.com>
2026-08-19 11:36:35 +0530

    Configure github pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 export default defineConfig({
-    base: './',
+    base: '/BarGraph/',
     plugins: [react()],
     server: {
         host: '0.0.0.0',
--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,15 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: './',
+  base: '/BarGraph/',
+
   plugins: [react()],
+
   server: {
     host: '0.0.0.0',
     port: 5173,
   },
+
   preview: {
     host: '0.0.0.0',
     port: 4173,
